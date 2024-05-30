b2d37f6ce5015e13dc391845973129439bab1fd2
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-05-30 17:11:10 +0800

    Add files via upload

---
+++ src/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div className="game-container">
+      <h1>Welcome to the Game!</h1>
+      {/* Game content goes here */}
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // For styling
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="title">Fraudulent Felines</h1>
+      <Link to="/game" className="start-button">Start</Link>
+      <p className="subtitle">be the purr-fect detective</p>
+    </div>
+  );
+}
+
+export default Home;
