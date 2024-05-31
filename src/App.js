332d42119bae0eac14e92eeb750c21b400e07541
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-06-01 01:29:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Game from './Game';
-import Winpage from './Winpage';
-import Losepage from './Losepage';
+import Phone from './Phone';
 import './App.css'; // For global styling
 
 function App() {
@@ -12,12 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
-        <Route path="/Winpage" element={<Winpage />} />
-        <Route path="/Losepage" element={<Losepage />} />
+        <Route path="/phone" element={<Phone />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
