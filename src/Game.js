d7e0b438cfe5aaaa8fd68602b8b717ac36827cac
winghyxn <163494488+winghyxn@users.noreply.github.com>
2024-05-31 23:08:37 +0800

    added meow

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import cat from "./images/Cat.png";
 import caseFileImage from "./images/file.png";
 import mic from "./images/mic.png"; 
 import checklist from "./images/checklist.png"; 
+import catAudio from './meow.mp3';
 
 function Game() {
   const initialCaseFileContent = "Message says “This is BOP (Bank of Pussy). There was a withdrawal of S$369 with your BOP account on 15 December at 20:31.\nIf unauthorised, visit https://bankofpussyhelpline.securesg.site to stop the process.”";
@@ -19,6 +20,14 @@ function Game() {
       {id: 4, label: "Called the lawyer for legal advice"},
       {id: 5, label: "Called the police after confirming it is a scam"},
   ]);
+
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (speaking && audioRef.current) {
+      audioRef.current.play();
+    }
+  }, [speaking]);
 
   const handleMicrophoneClick = () => {
     let catSpeak;
@@ -125,9 +134,11 @@ function Game() {
       <button onClick={() => setShowCaseFile(true)}>
         <img alt="case file" src={caseFileImage} width="50" height="50" />
       </button>
+      <audio ref={audioRef} src={catAudio} />
     </div>
   );
 }
 
 export default Game;
+
 
